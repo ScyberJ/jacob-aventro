a4272c94b1be29460139f128d270f8cd42cb670e
Scyberaid <82316458+ScyberJ@users.noreply.github.com>
2025-01-23 11:15:53 +0200

    Update urls.py
    Change url name to be more unique

--- jacob/insurance_brokerage/urls.py
+++ jacob/insurance_brokerage/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     path("", index,name="home"),
     path("cover/<str:slug>/", insurance_cover, name="insurance_cover"),
-    path("about-us", about_us, name="about_us")
+    path("about-us", about_us, name="ib_about_us")
 ]
