ef90785b1dbb49f47dd1fc9e14173dbf8b412f83
Aiden.J <kiddow12345@gmail.com>
2025-01-29 21:21:08 +0200

    Add app name that got lost in merge conflict

--- jacob/insurance_brokerage/urls.py
+++ jacob/insurance_brokerage/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
 from .views import *
 
-app_name='ib'
-
 urlpatterns = [
-    path("", index,name="insurance_brokerage"),
+    path("", index,name="home"),
     path("cover/<str:slug>/", insurance_cover, name="insurance_cover"),
-    path("about-us", about_us, name="ib_about_us")
+    path("about-us", about_us, name="about_us")
 ]
